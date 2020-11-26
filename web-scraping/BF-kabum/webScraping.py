c0bc330f528334a63f4dde513d7cca8cdfd00dbf
Emanuel Alvares Lima Camargo Ribeiro <emanuel.ribeiro@seduh.df.gov.br>
2020-11-26 15:04:52 -0300

    mudei pra tatica do json

--- web-scraping/BF-kabum/webScraping.py
+++ web-scraping/BF-kabum/webScraping.py
@@ -3,6 +3,7 @@ import json
 import lxml
 import requests
 import pandas as pd
+import urllib.request as request
 from lxml import html
 from produto import Produto
 from bs4 import BeautifulSoup
@@ -10,45 +11,34 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 
-url = "https://www.kabum.com.br/"
-
 procura_termo = str(input("\nInsira o nome do produto desejado: "))
 
-maior_desconto = 0.0
-menor_preco = 0.0
-produto_mais_barato = Produto("", "", "", "")
-melhor_custo_beneficio = Produto("", "", "", "")
-procura_termos = procura_termo.split(" ")
+url = (f'https://b2lq2jmc06.execute-api.us-east-1.amazonaws.com/PROD/ofertas?campanha=blackfriday&app=1&limite=200&pagina=1&string={procura_termo}')
 
-option = Options()
-option.headless = True                          # True = o navegador não abre, False = o navegador abre
-driver = webdriver.Firefox(options=option)
-option.add_argument('--ignore-certificate-errors')
-option.add_argument('--incognito')
-option.add_argument('--headless')
+with request.urlopen(url) as response:
+  
+  if (response.getcode() == 200):
+    source = response.read()
+    data = json.loads(source)
+    print(data)
 
-driver.get(url)
+    #dict_keys(['quant_ofertas', 'categorias', 'logar', 'produtos', 'oferta', 'ordem', 'encerradas', 'filtro', 'quant_paginas'])
+  
+  else:
+    print("Ocorreu um erro na tentativa de obter os dados da API")
 
-time.sleep(1)                                  # sleep de 10 segundos pra ter certeza qua a pagina carregou completamente
+produtos = [produto for produto in data['produtos']]
 
-driver.find_element_by_xpath("//div[@id='BlocoBannerTopo']//a").click()
+with open('produtos_kbm.json', 'w') as f:
+  json.dump(produtos, f, indent = 4, sort_keys = True)
 
-time.sleep(2)
+with open('produtos_kbm.json') as f:
+  data = json.load(f)
 
-elemento = driver.find_element_by_xpath("//div[@class='bloco-bg']//input[@id='input-busca']")
-elemento.send_keys(procura_termo)
-elemento.send_keys(Keys.ENTER)
+def lista_produtos(produto):
+  return(produto['produto'], produto['vlr_normal'], produto['vlr_oferta'])
 
-produtos = []
+with open('produtos_kbm.json') as f:
+  data = json.load(f, object_hook= lista_produtos)
 
-time.sleep(1)
-
-continua = 1
-
-for i in range(20): #trocar por um for que saiba onde parar(ainda nao faço ideia como)
-  nome = elemento.find_elements_by_xpath("//div[@class='produto-nome']")[i].text
-  preco = elemento.find_elements_by_xpath("//span[@class='produto-avista']")[i].text
-  preco_anterior = elemento.find_elements_by_xpath("//span[@class='produto-aprazo']//s")[i].text
-  print(f"{nome}\nCusta: {preco}\nCustava: {preco_anterior}")
-  # Resolver o problema de retornar somente 3 produtos (Tambem nao faço ideia como)
-driver.quit()
+#https://www.pluralsight.com/guides/importing-data-from-json-resource-with-python
